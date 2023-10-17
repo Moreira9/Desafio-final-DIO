13f68c6a84cdccdbde94fb0b933dd38b79ec0c7f
Matheus Moreira <matheus123321asd@gmail.com>
2023-10-17 14:58:38 -0300

    Exercio 2 comcluido

---
+++ Exercicio - Classe abstrata 1 e 2/src/Exercicio2/FormaGeometrica.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio2;
+
+/**
+ *
+ * @Matheus Moreira de Arruda
+ */
+public abstract class FormaGeometrica  {
+    
+    public abstract double calcularArea();
+    
+}
